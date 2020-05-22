46c1baee34d6f1765de308e512263b72de518b7c
Nicholas Moore <nsmoore57@gmail.com>
2020-05-21 19:05:59 -0500

    Add epGreedyPolicy

--- src/MoutianClimberDDQN.jl
+++ src/MoutianClimberDDQN.jl
@@ -6,9 +6,29 @@ using Reinforce.MountainCarEnv: MountainCar
 using Plots
 gr()
 
-mutable struct BasicCarPolicy <: Reinforce.AbstractPolicy end
+mutable struct ϵGreedyPolicy <: Reinforce.AbstractPolicy
+    ϵ::AbstractFloat
+    greedy::Reinforce.AbstractPolicy
+end
 
-Reinforce.action(policy::BasicCarPolicy, r, s, A) = s.velocity < 0 ? 1 : 3
+function Reinforce.action(policy::ϵGreedyPolicy, r, s, A)
+    rand(1) < policy.ϵ ? rand(A) : action(policy.greedy, r, s, A)
+end
+
+function Reinforce.reset!(policy::ϵGreedyPolicy)
+    reset!(policy.greedy)
+    # Leave the ϵ value alone since we want it to be preserved between episodes
+end
+
+mutable struct QLearnPolicy <: Reinforce.AbstractPolicy
+    ns::Int # Dimension of State
+end
+
+function Reinforce.action(policy::QLearnPolicy, r, s, A)
+    s.velocity < 0 ? 1 : 3
+end
+
+function build_QLearnPolicy(state_dim::Int)
 
 env = MountainCar()
 
@@ -21,6 +41,6 @@ function episode!(env, π = RandomPolicy())
     ep.total_reward, ep.niter
 end
 
-R, n = episode!(env, BasicCarPolicy())
+R, n = episode!(env, QLearnPolicy())
 println("reward: $R, iter: $n")
 end # module
