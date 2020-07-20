417825bd23839a9453a7d4f97137bf7f2525adae
Nicholas Moore <nsmoore57@gmail.com>
2020-07-19 19:13:16 -0500

    Miscellaneous Code Clean-Up

--- src/policies.jl
+++ src/policies.jl
@@ -1,18 +1,3 @@
-function action(policy::MooreDDQN.DeepQPolicy, r, s::MountainCarState, A)
-    inputs = [s.position s.velocity]'
-    argmax(policy.nn(inputs))[1]
-end
-
-function action(policy::MooreDDQN.Double_DeepQPolicy, r, s::MountainCarState, A)
-    inputs = [s.position s.velocity]'
-    argmax(policy.primaryNetwork(inputs))[1]
-end
-
-function action(policy::MooreDDQN.DuelingDouble_DeepQPolicy, r, s::MountainCarState, A)
-    inputs = [s.position s.velocity]'
-    argmax(get_QValues(policy,inputs))[1]
-end
-
 # HandPolicy -------------------------------------------------------
 mutable struct HandPolicy <: AbstractPolicy end
 
